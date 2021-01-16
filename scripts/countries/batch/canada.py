b2438366f5a30429606c6ed9a4115ac2c4b0d62c
lucas rg <lucasrg@kth.se>
2021-01-16 13:35:35 +0100

    fixed date field in Canada

--- scripts/countries/batch/canada.py
+++ scripts/countries/batch/canada.py
@@ -3,13 +3,14 @@ import pandas as pd
 
 def main():
     url = "https://raw.githubusercontent.com/ccodwg/Covid19Canada/master/timeseries_prov/vaccine_administration_timeseries_prov.csv"
-    df = pd.read_csv(url, parse_dates=["date_vaccine_administered"])
+    df = pd.read_csv(url)
 
     df = df.rename(columns={
         "date_vaccine_administered": "date",
         "province": "region",
         "cumulative_avaccine": "total_vaccinations"
     })
+    df.loc[:, "date"] = pd.to_datetime(df.loc[:, "date"], format="%d-%m-%Y")
     df.loc[:, "country"] = "Canada"
     df.loc[:, "date"] = df.loc[:, "date"].dt.strftime("%Y-%m-%d")
     df = df[["date", "country", "region", "total_vaccinations"]]

2a80323030eba8c516ab10d609cc5a21626af33c
lucas rg <lucasrg@kth.se>
2021-01-17 22:44:46 +0100

    standarize region names

--- scripts/countries/batch/germany.py
+++ scripts/countries/batch/germany.py
@@ -1,13 +1,20 @@
 import pandas as pd
 
 
+replace = {
+    "Baden-Württemberg": "Baden-Wurttemberg"
+    "Thüringen": "Thuringen"
+}
+
+
 def main():
     url = "https://raw.githubusercontent.com/mathiasbynens/covid-19-vaccinations-germany/main/data/data.csv"
-    df = pd.read_csv(url, usecols=["date", "state", "vaccinationsCumulative"], parse_dates=["date"])
+    df = pd.read_csv(url, usecols=["date", "state", "vaccinationsCumulative"])
     df = df.rename(columns={
         "state": "region",
         "vaccinationsCumulative": "total_vaccinations",
     })
+    df.loc[:, "region"] = df.loc[:, "region"].replace(replace)
     df.loc[:, "date"] = pd.to_datetime(df.loc[:, "date"], format="%Y-%m-%d")
     df.loc[:, "date"] = df.loc[:, "date"].dt.strftime("%Y-%m-%d")
     df.loc[:, "location"] = "Germany"

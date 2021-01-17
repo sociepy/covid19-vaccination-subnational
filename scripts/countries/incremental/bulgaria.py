3631ba429fcaa159279a4f9635327a1cabc51005
lucas rg <lucasrg@kth.se>
2021-01-17 23:01:21 +0100

    standarize region names

--- scripts/countries/incremental/bulgaria.py
+++ scripts/countries/incremental/bulgaria.py
@@ -7,6 +7,36 @@ import datetime
 
 
 source_file = "data/countries/Bulgaria.csv"
+replace = {
+    "Благоевград": "Blagoevgrad",
+    "Бургас": "Burgas",
+    "Варна": "Varna",
+    "Велико Търново": "Veliko Tarnovo",
+    "Видин": "Vidin",
+    "Враца": "Vratsa",
+    "Габрово": "Gabrovo",
+    "Добрич": "Dobrich",
+    "Кърджали": "Kardzhali",
+    "Кюстендил": "Kyustendil",
+    "Ловеч": "Lovech",
+    "Монтана": "Montana",
+    "Пазарджик": "Pazardzhik",
+    "Перник": "Pernik",
+    "Плевен": "Pleven",
+    "Пловдив": "Plovdiv",
+    "Разград": "Razgrad",
+    "Русе": "Ruse",
+    "Силистра": "Silistra",
+    "Сливен": "Sliven",
+    "Смолян": "Smolyan",
+    "София": "Sofia",
+    "София (столица)": "Sofia (stolitsa)",
+    "Стара Загора": "Stara Zagora",
+    "Търговище": "Targovishte",
+    "Хасково": "Haskovo",
+    "Шумен": "Shumen",
+    "Ямбол": "Yambol"
+}
 
 
 def main():
@@ -27,7 +57,8 @@ def main():
             "Област": "region",
             "Общо": "total_vaccinations"
         })
-
+        df = df[df.loc[:, "region"] != "Общо"]
+        df.loc[:, "region"] = df.loc[:, "region"].replace(replace)
         df.loc[:, "date"] = date
         df.loc[:, "location"] = "Bulgaria"
         df = df[["date", "location", "region", "total_vaccinations"]]

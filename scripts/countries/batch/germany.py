cb972be13c6ea88f97c9e912a369fbee86d93bb9
lucas rg <lucasrg@kth.se>
2021-01-18 18:28:46 +0100

    Support new RKI spreadsheet structure, as reported in mathiasbynens/covid-19-vaccinations-germany

--- scripts/countries/batch/germany.py
+++ scripts/countries/batch/germany.py
@@ -10,11 +10,13 @@ replace = {
 
 def main():
     url = "https://raw.githubusercontent.com/mathiasbynens/covid-19-vaccinations-germany/main/data/data.csv"
-    df = pd.read_csv(url, usecols=["date", "state", "vaccinationsCumulative"])
+    df = pd.read_csv(url, usecols=["date", "state", "firstDosesCumulative", "secondDosesCumulative"])
     df = df.rename(columns={
         "state": "region",
-        "vaccinationsCumulative": "total_vaccinations",
+        "firstDosesCumulative": "people_vaccinated",
+        "secondDosesCumulative": "people_fully_vaccinated"
     })
+    df.loc[:, "total_vaccinations"] = df.loc[:, "people_vaccinated"] + df.loc[:, "people_fully_vaccinated"]
     df.loc[:, "region"] = df.loc[:, "region"].replace(replace)
     df.loc[:, "date"] = pd.to_datetime(df.loc[:, "date"], format="%Y-%m-%d")
     df.loc[:, "date"] = df.loc[:, "date"].dt.strftime("%Y-%m-%d")
@@ -22,7 +24,8 @@ def main():
     # Add ISO codes
     df = merge_iso(df, country_iso="DE")
     # Reorder columns
-    df = df[["location", "region", "date", "location_iso", "region_iso", "total_vaccinations"]]
+    df = df[["location", "region", "date", "location_iso", "region_iso", 
+             "total_vaccinations", "people_vaccinated", "people_fully_vaccinated"]]
     df = df.sort_values(by=["region", "date"])
     df.to_csv("data/countries/Germany.csv", index=False)
 

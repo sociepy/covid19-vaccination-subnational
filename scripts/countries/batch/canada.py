037cbc35f9c9f0108be469dcb9c38d7426024025
lucas rg <lucasrg@kth.se>
2021-01-20 12:59:12 +0100

    add 2 dose info for canada #2

--- scripts/countries/batch/canada.py
+++ scripts/countries/batch/canada.py
@@ -27,8 +27,26 @@ def main():
     df.loc[:, "location"] = "Canada"
     # Add ISO codes
     df = merge_iso(df, country_iso="CA")
+    # Add completed vaccinations
+    url = "https://raw.githubusercontent.com/ccodwg/Covid19Canada/master/timeseries_prov/vaccine_completion_timeseries_prov.csv"
+    df_2 = pd.read_csv(url)
+
+    df_2 = df_2.rename(columns={
+        "date_vaccine_completed": "date",
+        "province": "region",
+        "cumulative_cvaccine": "people_fully_vaccinated"
+    })
+    # Date
+    df_2.loc[:, "date"] = pd.to_datetime(df_2.loc[:, "date"], format="%d-%m-%Y")
+    df_2.loc[:, "date"] = df_2.loc[:, "date"].dt.strftime("%Y-%m-%d")
+    # New cols
+    df_2.loc[:, "region"] = df_2.loc[:, "region"].replace(replace)
+    df = df.merge(df_2, on=["region", "date"], how="left")
+    df.loc[:, "people_fully_vaccinated"] = df.loc[:, "people_fully_vaccinated"].fillna(0).astype(int)
+    df.loc[:, "people_vaccinated"] = df.loc[:, "total_vaccinations"] - df.loc[:, "people_fully_vaccinated"].astype(int)
     # Reorder columns
-    df = df[["location", "region", "date", "location_iso", "region_iso", "total_vaccinations"]]
+    df = df[["location", "region", "date", "location_iso", "region_iso", 
+             "total_vaccinations", "people_vaccinated", "people_fully_vaccinated"]]
     df = df.sort_values(by=["region", "date"])
     df.to_csv("data/countries/Canada.csv", index=False)
 

05a729d59fe04cd35783a1739e903aedf26e6bf3
lucas rg <lucasrg@kth.se>
2021-01-31 19:34:27 +0100

    refactor

--- src/covid_updater/iso.py
+++ src/covid_updater/iso.py
@@ -13,20 +13,16 @@ ISO_NEW_FILE = os.path.join(this_directory, "assets/ISO_3166_2.csv")
 
 
 class ISODB():
-    def __init__(self):
-        self.__file = ISO_NEW_FILE
+    def __init__(self, filepath=ISO_NEW_FILE, df=None):
+        self._file = filepath
+        if isinstance(df, pd.DataFrame):
+            if os.path.isfile(self._file):
+                self._df = pd.read_csv(self._file)
+            else:
+                raise Exception("DB file not created. Run `create_from_source` before.")
 
-    def as_df(self):
-        """Load DB as DataFrame.
-
-        Returns:
-            pandas.DataFrame: ISO codes as DataFrame.
-        """
-        if os.path.isfile:
-            return pd.read_csv(self.__file)
-        raise Exception("DB file not created. Run `create_from_source` before.")
-
-    def create_from_source(self, source_file=ISO_ORIGINAL_FILE):
+    @classmethod
+    def create_from_source(cls, source_file=ISO_ORIGINAL_FILE, filepath=ISO_NEW_FILE):
         """Create ISO db.
 
         Uses IP2LOCATION-ISO3166-2 file.
@@ -39,15 +35,18 @@ class ISODB():
             "country_code": "location_iso",
             "code": "region_iso"
         })
-        df_iso = df_iso.sort_values(["location_iso", "region_iso"])
-        df_iso.to_csv(self.__file, index=False)
+        df = df.sort_values(["location_iso", "region_iso"])
+        df.to_csv(filepath, index=False)
+        return cls(filepath=filepath, df=df)
 
     def append(self, items):
-        df = self.as_df()
         new_items = pd.DataFrame(items, columns=["location_iso", "region_iso", "subdivision_name"])
-        df_iso = df_iso.append(new_items, ignore_index=True)
-        df_iso = df_iso.sort_values(["location_iso", "region_iso"])
-        df_iso.to_csv(self.__file, index=False)
+        self._df = self._df.append(new_items, ignore_index=True)
+        self._df = self._df.sort_values(["location_iso", "region_iso"])
+        self._df.to_csv(self._file, index=False)
+
+    def rename_values(self, colname, rename_dix):
+        self._df.loc[:, colname] = self._df.loc[:, colname].replace(rename_dix)
 
     def merge(self, df, mode="region_iso", country_iso=None):
         """Merge input dataframe with ISO database.
@@ -62,7 +61,7 @@ class ISODB():
         Returns:
             pandas.DataFrame: Joined table.
         """
-        df_iso = self.as_df()
+        df_iso = self._df
         if mode == "region_iso" and country_iso is not None:
             df_iso_country = df_iso[df_iso["location_iso"]==country_iso]
             df = df.merge(df_iso_country, left_on="region", right_on="subdivision_name", how="left")
@@ -77,47 +76,3 @@ class ISODB():
             raise ValueError("Not valid `mode` value. Choose either 'region_iso' or 'region'. If 'region_iso'," + \
                              "make sure to set a value for `country_iso`.")
         return df
-
-
-def generate_iso():
-    """Update ISO file."""
-    df_iso = pd.read_csv(ISO_ORIGINAL_FILE)
-    df_iso = df_iso.rename(columns={
-        "country_code": "location_iso",
-        "code": "region_iso"
-    })
-    # Change names
-    iso_replace = {
-        "Friuli-Venezia Giulia": "Friuli Venezia Giulia",
-        "Brussels Hoofdstedelijk Gewest": "Brussels"
-    }
-    df_iso.loc[:, "subdivision_name"] = df_iso.loc[:, "subdivision_name"].replace(iso_replace)
-
-    # Add new elements
-    new_items = [
-        ["IT", "IT-TN", "Provincia autonoma di Trento"],
-        ["IT", "IT-BZ", "Provincia autonoma di Bolzano - Alto Adige"],
-        ["FR", "FR-RE", "La Reunion"],
-        ["FR", "FR-YT", "Mayotte"],
-        ["BE", "BE-VLG", "Flanders"],
-        ["BE", "BE-WAL", "Wallonia"],
-        ["US", "US-AS", "American Samoa"],
-        ["US", "US-GU", "Guam"],
-        ["US", "US-VI", "Virgin Islands"],
-        ["US", "US-MP", "Northern Mariana Islands"],
-        ["FR", "FR-MQ", "Martinique"],
-        ["FR", "FR-GP", "Guadeloupe"],
-        ["FR", "FR-GF", "Guyane"],
-        ["NO", "NO-46", "Vestland"],
-        ["NO", "NO-42", "Agder"],
-        ["NO", "NO-30", "Viken"],
-        ["NO", "NO-54", "Troms og Finnmark"],
-        ["NO", "NO-50", "Trondelag"],
-        ["NO", "NO-38", "Vestfold og Telemark"],
-        ["NO", "NO-34", "Innlandet"],
-        ["CL", "CL-NB", "Nuble"]
-    ]
-    new_items = pd.DataFrame(new_items, columns=["location_iso", "region_iso", "subdivision_name"])
-    df_iso = df_iso.append(new_items, ignore_index=True)
-    df_iso = df_iso.sort_values(["location_iso", "region_iso"])
-    df_iso.to_csv(ISO_NEW_FILE, index=False)

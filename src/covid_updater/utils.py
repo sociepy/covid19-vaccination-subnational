4139295636f64f7d819b20aa7a6927583581c98b
lucas rg <lucasrg@kth.se>
2021-02-01 01:57:16 +0100

    xlsx loader

--- src/covid_updater/utils.py
+++ src/covid_updater/utils.py
@@ -1,4 +1,4 @@
-import urllib
+import urllib.request
 import pandas as pd
 from covid_updater.tracking import update_country_tracking
 
@@ -10,12 +10,25 @@ COLUMNS_INT = ["total_vaccinations", "people_vaccinated", "people_fully_vaccinat
 
 
 def read_xlsx_from_url(url, tmp_file="tmp/file.xlsx"):
+    """Download and load xls file from URL.
+
+    Args:
+        url (str): File url.
+        tmp_file (str, optional): Local temporal filename. Defaults to "tmp/file.xlsx".
+
+    Returns:
+        pandas.DataFrame: Data loaded.
+    """
     headers = {'User-Agent': "Mozilla/5.0 (X11; Linux i686)"}
     req = urllib.request.Request(url, headers=headers)
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
-    with open(tmp_file, "wb") as f:
-        f.write(the_page)
+    try:
+        with open(tmp_file, "wb") as f:
+            f.write(the_page)
+    except:
+        raise Exception("Make sure that folders leading to `tmp_file` exist. You may try to create a folder named" + \
+                        "`tmp` in the project root directory and re-run this.")
     df = pd.read_excel(tmp_file)
     return df
 

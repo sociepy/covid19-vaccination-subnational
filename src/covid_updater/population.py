3f67b864c57a5ff2f9ec3a25be43ddc249d6288a
lucas rg <lucasrg@kth.se>
2021-02-06 00:38:05 +0100

    module to get population

---
+++ src/covid_updater/population.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import qwikidata
+import qwikidata.sparql
+import pandas as pd
+
+
+def get_population(region_iso_list: list):
+    """Get population from given regions.
+
+    Regions identified by `region_iso`.
+    
+    Args:
+        region_iso_list (list): Iterable with iso codes.
+
+    Returns:
+        pandas.DataFrame: Retrieved data. Two columns: region_iso, population
+    """
+    query = """
+    SELECT ?item ?value ?population
+    WHERE
+    {
+      #?item wdt:P300 "AR-U".
+      ?item wdt:P1082 ?population.
+      ?item wdt:P300 ?value .
+      FILTER(?value IN %s)
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". }
+    }
+    """ % str(tuple(region_iso_list))
+    res = qwikidata.sparql.return_sparql_query_results(query)
+    res = [(item["value"]["value"], int(item["population"]["value"])) for item in res["results"]["bindings"]]
+    df = pd.DataFrame(res, columns=["region_iso", "population"])
+    df.loc[:, "date"] = datetime.now().date().strftime("%Y-%m-%d")
+    return df

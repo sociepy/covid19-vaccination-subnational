7a2ab2a5605d4a52faa6f00847add543c5a4d368
lucasrodes <lucasrodes@users.noreply.github.com>
2021-02-10 17:25:16 +0100

    function argument naming b ug

--- scripts/countries/incremental/denmark.py
+++ scripts/countries/incremental/denmark.py
@@ -4,7 +4,7 @@ from covid_updater.scraping import get_country_scraper
 def main():
     scraper = get_country_scraper(iso_code="DK")
     output_file = f"data/countries/{scraper.filename}.csv"
-    scraper.run(output_file=output_file)
+    scraper.run(output_file_data=output_file)
 
 
 if __name__ == "__main__":

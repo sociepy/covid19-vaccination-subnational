da3dd84ee189f2f4d649bded870c26ef174a6940
lucas rg <lucasrg@kth.se>
2021-02-20 18:43:43 +0000

    switzerland 2 dose #36

--- src/covid_updater/scraping/countries/switzerland.py
+++ src/covid_updater/scraping/countries/switzerland.py
@@ -16,17 +16,21 @@ class SwitzerlandScraper(Scraper):
             }, 
             column_renaming={
                 "geounit": "region_iso",
-                "ncumul_vacc": "total_vaccinations"
+                "ncumul_vacc": "total_vaccinations",
+                "ncumul_fully_vacc": "people_fully_vaccinated"
             },
             mode_iso_merge="region"
         )
 
     def load_data(self):
         # Load
-        df = pd.read_csv(self.data_url, usecols=["geounit", "date", "ncumul_vacc"])
-        df = df.loc[~df.loc[:, "geounit"].isin(["CHFL", "FL"])].reset_index(drop=True)
-        return df
+        df = pd.read_csv(self.data_url, usecols=["geounit", "date", "ncumul_vacc", "ncumul_fully_vacc"])
+        return df.loc[~df.loc[:, "geounit"].isin(["CHFL", "FL"])].reset_index(drop=True)
 
     def _process(self, df):
         df.loc[:, "region_iso"] = self.country_iso + "-" + df.loc[:, "region_iso"]
+        df.loc[:, "people_vaccinated"] = (
+            df.loc[:, "total_vaccinations"] - df.loc[:, "people_fully_vaccinated"]
+            .fillna(0)
+        )
         return df

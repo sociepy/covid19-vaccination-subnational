ad6171358c891c167a3f2e7bfc99e40b318fe780
lucas rg <lucasrg@kth.se>
2021-02-20 23:36:09 +0000

    iceland

--- src/covid_updater/scraping/countries/iceland.py
+++ src/covid_updater/scraping/countries/iceland.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import urllib.request
-from datetime import datetime
-import locale
+from datetime import datetime, timedelta
+import json
+import pytz
 from covid_updater.scraping.base import IncrementalScraper
 
 
@@ -11,8 +12,8 @@ class IcelandScraper(IncrementalScraper):
         super().__init__(
             country="Iceland", 
             country_iso="IS", 
-            data_url="://e.infogram.com/c3bc3569-c86d-48a7-9d4c-377928f102bf", 
-            data_url_reference="://e.infogram.com/c3bc3569-c86d-48a7-9d4c-377928f102bf", 
+            data_url="https://e.infogram.com/c3bc3569-c86d-48a7-9d4c-377928f102bf", 
+            data_url_reference="https://e.infogram.com/c3bc3569-c86d-48a7-9d4c-377928f102bf", 
             region_renaming={
                 "Höfuðborgarsvæði": "Hofudborgarsvaedi",
                 "Norðurland eystra": "Nordurland eystra",
@@ -36,8 +37,7 @@ class IcelandScraper(IncrementalScraper):
                 s = script.string.replace("window.infographicData=", "")[:-1]
                 return json.loads(s)
 
-    def _get_data_list_from_script(script):
-        script = find_script(scripts)
+    def _get_data_list_from_script(self, script):
         data = (
             script["elements"]["content"]["content"]["entities"]["8752e817-052d-4b0b-9985-52dfe3983bba"]
             ["props"]["chartData"]["data"][0]
@@ -49,23 +49,23 @@ class IcelandScraper(IncrementalScraper):
         script = self._find_script(soup)
         data = self._get_data_list_from_script(script)
         df = (
-            pd.DataFrame.from_records(ss[0][1:], columns=["region", "people_vaccinated", "people_fully_vaccinated"])
+            pd.DataFrame.from_records(data[1:], columns=["region", "people_vaccinated", "people_fully_vaccinated"])
         )
         df = df[~(df.region == "Óskráð")]
         return df
 
-    def _undo_per_100k(df, population_iceland=None):
+    def _undo_per_100k(self, df, population_iceland=None):
         if population_iceland is None:
             population_iceland = [
                 ["Austurland", 13173],
-                ["Höfuðborgarsvæði", 233034],
-                ["Norðurland eystra", 30600],
-                ["Norðurland vestra", 7322],
-                ["Suðurland", 28399],
-                ["Suðurnes", 27829],
-                ["Vestfirðir", 7115],
+                ["Hofudborgarsvaedi", 233034],
+                ["Nordurland eystra", 30600],
+                ["Nordurland vestra", 7322],
+                ["Sudurland", 28399],
+                ["Sudurnes", 27829],
+                ["Vestfirdir", 7115],
                 ["Vesturland", 16662],
-                ["Norðurland", 30600+7322]
+                ["Nordurland", 30600+7322]
             ]
         df_pop = pd.DataFrame(population_iceland, columns=["region", "population"])
         df = df.merge(df_pop, on="region")
@@ -76,13 +76,13 @@ class IcelandScraper(IncrementalScraper):
     def _process(self, df):
         df = self._undo_per_100k(df)
         df.loc[:, "total_vaccinations"] = df.loc[:, "people_vaccinated"] + df.loc[:, "people_fully_vaccinated"]
-        df.loc[:, "date"] = (
-            datetime.now(pytz.timezone("Europe/Sofia")).date() - timedelta(days=1)
-        ).strftime("%Y-%m-%d")
+        df = df.assign(
+            date=(datetime.now(pytz.timezone("Atlantic/Reykjavik")).date() - timedelta(days=1)).strftime("%Y-%m-%d")
+        )
         return df
 
     def _postprocess(self, df):
         df = super()._postprocess(df)
         df = df.assign(location_iso=self.country_iso)
-        #df.loc[df.region=="Nordurland", "region_iso"] = "IS-6"  # Also accounts for IS-5 ! TODO
+        df.loc[df.region=="Nordurland", "region_iso"] = "IS-6,IS-7"  # Also accounts for IS-5 ! TODO
         return df

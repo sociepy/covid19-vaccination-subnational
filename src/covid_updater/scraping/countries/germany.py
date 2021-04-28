c106897b8120d37b9d84dc360dadb45add9af206
Mathias Bynens <mathias@qiwi.be>
2021-04-28 16:21:05 +0200

    Update processing for German data per upstream changes
    Issue: #63

--- src/covid_updater/scraping/countries/germany.py
+++ src/covid_updater/scraping/countries/germany.py
@@ -15,8 +15,8 @@ class GermanyScraper(Scraper):
             },
             column_renaming={
                 "state": "region",
-                "firstDosesCumulative": "people_vaccinated",
-                "secondDosesCumulative": "people_fully_vaccinated",
+                "initialDosesCumulative": "people_vaccinated",
+                "finalDosesCumulative": "people_fully_vaccinated",
             },
         )
 
@@ -24,7 +24,7 @@ class GermanyScraper(Scraper):
         # Load
         df = pd.read_csv(
             self.data_url,
-            usecols=["date", "state", "firstDosesCumulative", "secondDosesCumulative"],
+            usecols=["date", "state", "initialDosesCumulative", "finalDosesCumulative"],
         )
         return df
 
